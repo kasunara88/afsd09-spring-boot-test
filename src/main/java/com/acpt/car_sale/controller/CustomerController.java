81b87f5c594464b67dadac8128bc554cdef18660
kasunara88 <wmaravinda@gmail.com>
2025-08-21 12:56:09 +0530

    Customer Controller route is done.

---
+++ src/main/java/com/acpt/car_sale/controller/CustomerController.java
@@ -0,0 +1,10 @@
+package com.acpt.car_sale.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/customer")
+public class CustomerController {
+    //Imagine code is done
+}
